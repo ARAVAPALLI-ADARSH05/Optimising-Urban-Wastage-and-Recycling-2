6a1db332553197c8adf7be9366171d73ba816d49
ARAVAPALLI ADARSH <aravapalliadarsh05@gmail.com>
2025-08-22 12:50:32 +0530

    Update index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ctrlMain = require('../controllers/main');
+const ctrlMain = require('../controlers/main');
 
 /* GET homepage. */
 router.get('/', ctrlMain.index);
